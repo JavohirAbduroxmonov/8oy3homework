91f255bbc742e14fae2687141cc428eec83a6edd
Javohir <JavohirAbduroxmonov>
2024-08-24 09:57:06 +0500

    csdvsdb

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,10 @@ import { HashLoader } from "react-spinners";
 import { Toaster } from "react-hot-toast";
 import { Context } from "./context/Provider.js";
 
-interface LoadingState {
-  loading: boolean;
-  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
-}
-
 const App = () => {
-  const [loading, setLoading] = useState<LoadingState | boolean>(true);
+  const [loading, setLoading] = useState(true);
   const context = useContext(Context);
+
   if (!context) {
     throw new Error("CountContext should be used inside CountProvider");
   }
